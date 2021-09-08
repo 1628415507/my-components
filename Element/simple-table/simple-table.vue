247c33c53e531c7e9cf2de16843513ac23143af6
blank <1628415507@qq.com>
2021-09-08 17:35:33 +0800

    fix：simple-table-添加min-Width

--- Element/simple-table/simple-table.vue
+++ Element/simple-table/simple-table.vue
@@ -1,85 +1,90 @@
 <!--
  * @Author: your name
  * @Date: 2021-06-08 15:26:46
- * @LastEditTime: 2021-08-25 16:08:30
+ * @LastEditTime: 2021-09-08 17:31:04
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \rfkt-admin\src\views\basicInformation\course\components\simple-table.vue
 -->
 <template>
-    <div class="simple-table">
-        <el-table
-            v-loading="loading"
-            ref="table"
-            style="width: 100%"
-            :size="size"
-            :data="tableData"
-            :height="height"
-            @selection-change="handleSelectionChange"
-            :header-cell-style="headerCellStyle"
+  <div class="simple-table">
+    <el-table
+      v-loading="loading"
+      ref="table"
+      style="width: 100%"
+      :size="size"
+      :data="tableData"
+      :height="height"
+      @selection-change="handleSelectionChange"
+      :header-cell-style="headerCellStyle"
+    >
+      <!-- 1.多选列 -->
+      <el-table-column v-if="selectShow" type="selection" fixed="left" header-align="center" align="center" width="50" :selectable="handleSelectAble" ></el-table-column>
+      <!-- 2.序号列 -->
+      <el-table-column v-if="indexShow" type="index" label="序号" fixed="left" width="48" header-align="center" align="center" ></el-table-column>
+      <template v-for="(item, index) in propData">
+        <!-- 3.基础列 -->
+        <el-table-column
+          v-if="!item.type"
+          :key="item.prop + index"
+          :prop="item.prop"
+          :label="item.label"
+          :width="item.width"
+          :min-width="item.minWidth"
+          :header-align="item.headerAlign || 'center'"
+          :align="item.align || 'center'"
+          :formatter="item.isFormatter ? formatVal : null"
+          show-overflow-tooltip
         >
-            <!-- 1.多选列 -->
-            <el-table-column v-if="selectShow" type="selection"  fixed="left" header-align="center" align="center" width="50" :selectable="handleSelectAble"></el-table-column>
-            <!-- 2.序号列 -->
-            <el-table-column v-if="indexShow" type="index" label="序号" fixed="left" width="48" header-align="center" align="center"></el-table-column>
-            <template  v-for="(item, index) in propData" >
-            <!-- 3.基础列 -->
-            <el-table-column
-                v-if="!item.type"
-                :key="index"
-                :prop="item.prop"
-                :label="item.label"
-                :width="item.width"
-                :header-align="item.headerAlign || 'center'"
-                :align="item.align || 'center'"
-                :formatter="item.isFormatter ? formatVal : null"
-                show-overflow-tooltip
-            >
-            </el-table-column>
-            <!-- 4.自定义列 -->
-            <el-table-column
-                v-if="item.type"
-                :key="index"
-                :prop="item.prop"
-                :label="item.label"
-                :width="item.width"
-                :header-align="item.headerAlign || 'center'"
-                :align="item.align || 'center'"
-                show-overflow-tooltip
-            >
-                <template slot-scope="scope">
-                    <!-- 图片列 -->
-                    <div v-if="item.type === 'img'">
-                        <el-image
-                            :src="$util.photoShow(scope.row[item.prop])"
-                            class="img" fit="contain">
-                            <div slot="error" class="image-slot"><i class="el-icon-warning-outline"></i></div>
-                        </el-image>
-                    </div>
-                    <!-- 插槽列（图片、按钮...） -->
-                    <div v-if="item.type === 'slot'">
-                        <slot :name="`${item.prop}`" v-bind:row="scope.row"></slot><!-- 插槽名：item.prop -->
-                    </div>
-                </template>
-            </el-table-column>
+        </el-table-column>
+        <!-- 4.自定义列 -->
+        <el-table-column
+          v-if="item.type"
+          :key="item.prop + index"
+          :prop="item.prop"
+          :label="item.label"
+          :width="item.width"
+          :min-width="item.minWidth"
+          :header-align="item.headerAlign || 'center'"
+          :align="item.align || 'center'"
+        >
+          <template slot-scope="scope">
+            <!-- 图片列 -->
+            <template v-if="item.type === 'img'">
+              <el-image
+                :src="$util.photoShow(scope.row[item.prop])"
+                class="img"
+                fit="contain"
+              >
+                <div slot="error" class="image-slot">
+                  <i class="el-icon-warning-outline"></i>
+                </div>
+              </el-image>
             </template>
+            <!-- 插槽列（图片、按钮...） 插槽名：item.prop  -->
+            <template v-if="item.type === 'slot'">
+              <slot :name="`${item.prop}`" v-bind:row="scope.row"></slot>
+            </template>
+          </template>
+        </el-table-column>
+      </template>
 
-            <!-- 5.操作列 -->
-            <el-table-column
-                v-if="operateShow"
-                label="操作"
-                header-align="center"
-                align="center"
-                fixed="right"
-                minWidth="130"
-            >
-                <template slot-scope="scope">
-                    <!-- 插槽名：operate，通过v-bind:row传回当前行的值 -->
-                    <slot name="operate" v-bind:row="scope.row"></slot>
-                </template>
-            </el-table-column>
-        </el-table>
-    </div>
+      <!-- 5.操作列 -->
+      <el-table-column
+        v-if="operateShow"
+        label="操作"
+        header-align="center"
+        align="center"
+        fixed="right"
+        minWidth="130"
+      >
+        <template slot-scope="scope">
+          <!-- 插槽名：operate，通过v-bind:row传回当前行的值 -->
+          <slot name="operate" v-bind:row="scope.row"></slot>
+        </template>
+      </el-table-column>
+    </el-table>
+  </div>
 </template>
 
 <script>
@@ -91,7 +96,7 @@
  *        solt 自定义插槽，插槽名字为prop的名字
  *   其他：label、prop、width、headerAlign、align同el-table-columne原属性
  * }
-*/
+ */
     export default {
         props: {
             // 数据部分
@@ -137,7 +142,8 @@
                     return ['medium', 'small', 'mini'].includes(val) // 可选值:medium / small / mini
                 }
             },
-            headerCellStyle: { // 表头单元格的 style
+            // 表头单元格的 style
+            headerCellStyle: {
                 type: Object,
                 required: false,
                 default: () => {}
@@ -157,7 +163,7 @@
                     val: cellValue // 字段值
                 }
                 // return this.formatter(temp) // 将数据传入formatter()方法处理后返回
-                return this.formatter({ ...row, ...temp })// 传入scope.row的值
+                return this.formatter({ ...row, ...temp }) // 传入scope.row的值
             },
             // 当选择项发生变化时会触发该事件
             handleSelectionChange (selection) {
@@ -189,18 +195,18 @@
 </script>
 
 <style lang='less' scoped>
-.simple-table{
-    .img{
-        position: relative;
-        width:70px;
-        height: 70px;
-        vertical-align: middle;
-        /deep/ .image-slot {
-            width:70px;
-            height: 70px;
-            color: #b5b7bb;
-            font-size: 20px;
-        }
+.simple-table {
+  .img {
+    position: relative;
+    width: 70px;
+    height: 70px;
+    vertical-align: middle;
+    /deep/ .image-slot {
+      width: 70px;
+      height: 70px;
+      color: #b5b7bb;
+      font-size: 20px;
     }
+  }
 }
 </style>
